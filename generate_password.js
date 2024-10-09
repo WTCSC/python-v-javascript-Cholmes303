e655e2a5621287fb6156a5132dda076bbfc089a5
Cholmes303 <cj@jasonholmes.net>
2024-10-09 14:30:18 -0600

    Semicolons, semicolons, semicolons........

--- generate_password.js
+++ generate_password.js
@@ -1,5 +1,16 @@
 function generatePassword(length = 12) {
-    // complete me
+    let password = '';
+    // Defines characters as all letters (lower and uppercase) any decimal digit (base 10) and punctuation marks
+    let characters = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890_";
+    
+    for(let i = 1; i <= length; i++) {
+        // Selects random values from characters up to a set amount, being 12 and joins them in a string
+        let generated = Math.floor(Math.random() * characters.length + 1); 
+
+        password += characters.charAt(generated)
+    }
+    
+    return password;
 }
 
 module.exports = generatePassword;
