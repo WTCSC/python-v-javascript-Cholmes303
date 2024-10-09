7fb0e1481950372544223a6e76a94ddeb3d620d6
Cholmes303 <cj@jasonholmes.net>
2024-10-09 14:45:02 -0600

    Comments

--- generate_password.js
+++ generate_password.js
@@ -1,10 +1,11 @@
 function generatePassword(length = 12) {
+    // This is where the generated password will go.
     let password = '';
-    // Defines characters as all letters (lower and uppercase) any decimal digit (base 10) and punctuation marks.
+    // Defines characters as all letters (lower and uppercase), any decimal digit (base 10), and punctuation marks (underscores only in this case).
     let characters = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOOPASDFGHJKLZXCVBNM1234567890_";
     // Loops in the string characters in a limited range set by length variable.
     for(let i = 1; i <= length; i++) {
-        // Selects random values from characters up to a set amount, being 12 and joins them in a string.
+        // Selects random character in characters string
         let generated = Math.floor(Math.random() * characters.length); 
         // Adds from the string characters, at random, to the empty string password.
         password += characters.charAt(generated)
